7be0312ad5611b8faa6fffd13aa43b5d66b17c3a
francisco <francisc.barraguirre@protonmail.com>
2021-06-14 15:14:47 -0300

    Renombrar archivo y mover el afd aca
    Separamos el `main.py` en 3 archivos. Aca en `afd.py` metimos el automata finito determinista. Tambien cambiamos el formato de `grafo` para que sea el mismo que tenia el `anfd.py`

--- afd.py
+++ afd.py
@@ -1,5 +1,37 @@
 
+grafo = {
+    1: [(2, "0"), (3, "1")],
+    2: [(4, "1")],
+    3: [(4, "0")],
+    4: [(2, "1"), (1, "0")]
+}
+
+estados_iniciales = 1
+estados_finales = [4]
+
+def afd(nodo_inicial, palabra):
+
+    estado_actual = nodo_inicial
+    aceptada = False
+
+    # (mientras la palabra no sea vacia) y (podemos seguir avanzando por las transiciones del nodoActual por el simbolo)
+    while len(palabra) > 0 and any([t[1] == palabra[0] for t in grafo[estado_actual]]):
+
+        simbolo = palabra[0]
+
+        for trans in grafo[estado_actual]:
+            if simbolo == trans[1]:
+                estado_actual = trans[0]
+                palabra = palabra[1:]
+                print(palabra, estado_actual, trans)
+
+    if estado_actual in estados_finales:
+        aceptada = True
+        print(aceptada, ", palabra aceptada.")
+    else:
+        print(aceptada, ", palabra no aceptada.")
 
 
+afd(estados_iniciales, "01")
 
 
