112ab63c4da552a7e5874146f17bd2f5385585b8
francisco <francisc.barraguirre@protonmail.com>
2021-06-14 15:15:21 -0300

    Renombrar archivo y mover el afnd aca
    Separamos el `main.py` en 3 archivos. Aca en `afnd.py` metimos el automata finito no-determinista

--- afnd.py
+++ afnd.py
@@ -1,38 +1,97 @@
+
+import sys
+
+
+def LINE():
+    return sys._getframe(1).f_lineno
+
+
+# grafo = {
+#     0: [(0, "a"), (0, "b"), (1, "a")],
+#     1: [(2, "b")],
+#     2: [(3, "b")],
+#     3: []
+# }
+
 grafo = {
-    "-e0": [("a", "-e0"), ("a", "-e0"), ("b", "e1")],
-    "e1": [("b", "e2")],
-    "e2": [("b", "*e3")],
-    "*e3": []
+    0: [(1, "a"), (2, "a")],
+    1: [(3, "b")],
+    2: [(5, "b")],
+    3: [(4, "a")],
+    4: [(1, "b")],
+    5: [(2, "a")]
 }
 
-palabra = "aabb"
-aceptada = False
-nodoActual = "-e0"
+estado_inicial = 0
+estados_finales = [1, 2]
+ramificaciones = []
+
+print("<-- INICIO ALGORTIMO -->\n")
+def afnd(estado_inicial, palabra):
+
+    cola = [estado_inicial]
+    visitados = []
+    bifurcaciones = []
+
+    while len(cola) > 0:
+        # print("cola:", cola)
+        # print("visitados:", visitados)
+        estado_actual = cola.pop(0)
+        # print("estado actual:", estado_actual)
+
+        if palabra:
+            char = palabra[0]
+        elif not palabra:
+            break
+
+        print("estado actual:", estado_actual, "y char:", char)
+        # print("caracter:", char)
+
+        vecinos = [tupla[0] for tupla in grafo.get(estado_actual) if tupla[1] == char]
+        # si no hay vecinos
+        if not vecinos:
+            return
+
+        if len(vecinos) > 1:
+            # metodo rapido para insertar todos los vecinos en bifurcaciones
+            for e in vecinos:
+                bifurcaciones.append(e)
+
+            print("bifurcaciones por", char, "-->", bifurcaciones)
+            for estado in bifurcaciones:
+                print("INICIO RAMIFICACION POR ESTADO:", estado)
+                ramificacion = afnd(estado, palabra[1:])
+                ramificaciones.append(ramificacion)
+                print("ramificacion:", ramificacion)
+                print("FIN RAMIFICACION")
 
 
-bifurcaciones = []
+        else:
+            # print("vecinos:", vecinos)
 
-# (mientras la palabra no sea vacia) y (podemos seguir avanzando por las transiciones del nodoActual por el simbolo)
-while len(palabra) > 0 and any([t[0] == palabra[0] for t in grafo[nodoActual]]):
+            for estado in vecinos:
+                # print("linea:", LINE(), "cola:", cola, "visitados:", visitados)
+                if estado not in visitados and palabra:
+                    visitados.append(estado)
+                    cola.append(estado)
+                if estado in visitados and palabra:
+                    return afnd(estado, palabra[1:])
 
-    simbolo = palabra[0]
-
-    if [t[0] for t in grafo[nodoActual]].count(simbolo) > 1:
-        bifurcaciones.append([t for t in grafo[nodoActual] if t[0] == simbolo])
-
-    for trans in grafo[nodoActual]:
-        if simbolo == trans[0]:
-            nodoActual = trans[1]
             palabra = palabra[1:]
-            print(palabra, nodoActual, trans, "bifurcaciones:", bifurcaciones)
+
+    if (not palabra and estado_actual not in estados_finales) or palabra:
+        return False
+
+    elif not palabra and estado_actual in estados_finales:
+        return True
 
 
-if nodoActual[0] == "*":
-    aceptada = True
-    print(aceptada, ", palabra aceptada.")
+afnd(estado_inicial, "aba")
+print("<-- FIN ALGORITMO -->\n")
+if any(ramificaciones):
+    print("Resultado: cadena aceptada")
 else:
-    print(aceptada, ", palabra no aceptada.")
+    print("Resultado: cadena rechazada")
 
 
-####
 
