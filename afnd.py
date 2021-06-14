8e3c0dfa780ff8daf8c000a04178502a5d887d64
francisco <francisc.barraguirre@protonmail.com>
2021-06-14 16:37:57 -0300

    Comentamos `grafo` y `estados`, ademas:
    Cambiamos los parametros del afnd() para que tome a `bifurcaciones`, `visitados` y `ramificaciones` como variables globales, para que todos los caminos las compartan.

--- afnd.py
+++ afnd.py
@@ -13,25 +13,22 @@ def LINE():
 #     3: []
 # }
 
-grafo = {
-    0: [(1, "a"), (2, "a")],
-    1: [(3, "b")],
-    2: [(5, "b")],
-    3: [(4, "a")],
-    4: [(1, "b")],
-    5: [(2, "a")]
-}
+# grafo = {
+#     0: [(1, "a"), (2, "a")],
+#     1: [(3, "b")],
+#     2: [(5, "b")],
+#     3: [(4, "a")],
+#     4: [(1, "b")],
+#     5: [(2, "a")]
+# }
 
 estado_inicial = 0
-estados_finales = [1, 2]
-ramificaciones = []
+# estados_finales = [1, 2]
+# ramificaciones = []
 
-print("<-- INICIO ALGORTIMO -->\n")
-def afnd(estado_inicial, palabra):
+def afnd(grafo, estado_inicial, estados_finales, palabra, visitados, bifurcaciones, ramificaciones):
 
     cola = [estado_inicial]
-    visitados = []
-    bifurcaciones = []
 
     while len(cola) > 0:
         # print("cola:", cola)
@@ -55,16 +52,16 @@ def afnd(estado_inicial, palabra):
         if len(vecinos) > 1:
             # metodo rapido para insertar todos los vecinos en bifurcaciones
             for e in vecinos:
-                bifurcaciones.append(e)
+                if e not in bifurcaciones:
+                    bifurcaciones.append(e)
 
             print("bifurcaciones por", char, "-->", bifurcaciones)
             for estado in bifurcaciones:
                 print("INICIO RAMIFICACION POR ESTADO:", estado)
-                ramificacion = afnd(estado, palabra[1:])
+                ramificacion = afnd(grafo, estado, estados_finales, palabra[1:], visitados, bifurcaciones, ramificaciones)
                 ramificaciones.append(ramificacion)
                 print("ramificacion:", ramificacion)
                 print("FIN RAMIFICACION")
-
 
         else:
             # print("vecinos:", vecinos)
@@ -75,23 +72,24 @@ def afnd(estado_inicial, palabra):
                     visitados.append(estado)
                     cola.append(estado)
                 if estado in visitados and palabra:
-                    return afnd(estado, palabra[1:])
+                    return afnd(grafo, estado, estados_finales, palabra[1:], visitados, bifurcaciones, ramificaciones)
 
             palabra = palabra[1:]
 
-    if (not palabra and estado_actual not in estados_finales) or palabra:
-        return False
-
-    elif not palabra and estado_actual in estados_finales:
+    if not palabra and estado_actual in estados_finales:
         return True
 
+    else:
+        return False
 
-afnd(estado_inicial, "aba")
-print("<-- FIN ALGORITMO -->\n")
-if any(ramificaciones):
-    print("Resultado: cadena aceptada")
-else:
-    print("Resultado: cadena rechazada")
+
+
+# afnd(estado_inicial, "aba")
+# print("<-- FIN ALGORITMO -->\n")
+# if any(ramificaciones):
+#     print("Resultado: cadena aceptada")
+# else:
+#     print("Resultado: cadena rechazada")
 
 
 
